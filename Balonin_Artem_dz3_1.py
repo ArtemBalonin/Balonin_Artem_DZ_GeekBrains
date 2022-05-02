93a7a9ea4bd974348e53f97f086b1c926d4df7fa
Artem <a.balonin.52@mail.ru>
2022-05-02 19:34:48 +0300

    Homework

---
+++ Balonin_Artem_dz3_1.py
@@ -0,0 +1,17 @@
+def num_translate(number):
+    print(numbers.get(number.lower()))
+numbers = {
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+number = input('Введите число на английском: ')
+num_translate(number)
